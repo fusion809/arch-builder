3fcef135c8d066d14cd602d27fecdab74be94d2a
fusion809 <brentonhorne77@gmail.com>
2015-10-27 17:45:49 +1000

    Making adjustments to install-dde.sh

---
+++ install-dde.sh
@@ -0,0 +1,3 @@
+printf "[home_metakcahura_arch-deepin_Arch_Extra]\nSigLevel = Never\nServer = http://download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch\nServer = http://anorien.csc.warwick.ac.uk/mirrors/download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch" >> /etc/pacman.conf
+/usr/bin/pacman -S lightdm lightdm-deepin-greeter deepin deepin-extra
+printf "[SeatDefaults]\ngreeter-session=lightdm-deepin-greeter" >> /etc/lightdm/lightdm.conf
