844002b89527ef6b8050e2faf6954aa7a0bc62af
fusion809 <brentonhorne77@gmail.com>
2015-12-23 04:11:30 +1000

    Adding new script

---
+++ install-rmdrive.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+DISK='/dev/sdb'
+echo "Type in the hostname, followed by [ENTER]:"
+read FQDN
+echo "Type in the username, followed by [ENTER]:"
+read USERNAME
+KEYMAP='us'
+LANGUAGE='en_AU.UTF-8'
+echo "Type in the password, followed by [ENTER]:"
+read PASS
+PASSWORD=$(/usr/bin/openssl passwd -crypt "$PASS")
+TIMEZONE='Australia/Brisbane'
+
+CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
+ROOT_PARTITION="${DISK}1"
+TARGET_DIR='/mnt'
+
+echo "==> clearing partition table on ${DISK}"
+/usr/bin/sgdisk --zap ${DISK}
+
+echo "==> destroying magic strings and signatures on ${DISK}"
+/usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048
+/usr/bin/wipefs --all ${DISK}
+
+echo "==> creating /root partition on ${DISK}"
+/usr/bin/sgdisk --new=1:0:0 ${DISK}
+
+echo "==> setting ${DISK} bootable"
+/usr/bin/sgdisk ${DISK} --attributes=1:set:2
+
+echo '==> creating /root filesystem (ext4)'
+/usr/bin/mkfs.ext4 -F -m 0 -q -L root ${ROOT_PARTITION}
+
+echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
+/usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} ${TARGET_DIR}
+
+echo '==> bootstrapping the base installation'
+/usr/bin/pacstrap ${TARGET_DIR} base base-devel
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux
+/usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
+/usr/bin/sed -i 's/sda3/sda1/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
+/usr/bin/sed -i 's/TIMEOUT 50/TIMEOUT 10/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
+
+echo '==> generating the filesystem table'
+/usr/bin/genfstab -p ${TARGET_DIR} >> "${TARGET_DIR}/etc/fstab"
+
+echo '==> generating the system configuration script'
+/usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
+
+cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
+	echo '${FQDN}' > /etc/hostname
+	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
+	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
+	/usr/bin/locale-gen
+	/usr/bin/mkinitcpio -p linux
+	/usr/bin/usermod --password ${PASSWORD} root
+	# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
+	/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
+	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+	/usr/bin/systemctl enable sshd.service
+
+	useradd $USERNAME
+	mkhomedir_helper $USERNAME
+	passwd $USERNAME
+EOF
+
+echo '==> entering chroot and configuring system'
+/usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
+rm "${TARGET_DIR}${CONFIG_SCRIPT}"
+
+# http://comments.gmane.org/gmane.linux.arch.general/48739
+echo '==> adding workaround for shutdown race condition'
+/usr/bin/install --mode=0644 poweroff.timer "${TARGET_DIR}/etc/systemd/system/poweroff.timer"
+
+echo '==> installation complete!'
+/usr/bin/sleep 3
+/usr/bin/umount ${TARGET_DIR}
+/usr/bin/systemctl reboot
