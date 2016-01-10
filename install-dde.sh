be475231ac6e107750993ab2d8b0ab9532daa1ce
fusion809 <brentonhorne77@gmail.com>
2016-01-10 16:08:07 +1000

    Updating json

--- install-dde.sh
+++
@@ -1,3 +0,0 @@
-printf "[home_metakcahura_arch-deepin_Arch_Extra]\nSigLevel = Never\nServer = http://download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch\nServer = http://anorien.csc.warwick.ac.uk/mirrors/download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch" >> /etc/pacman.conf
-/usr/bin/pacman -Sy lightdm lightdm-deepin-greeter deepin deepin-extra
-printf "[SeatDefaults]\ngreeter-session=lightdm-deepin-greeter" >> /etc/lightdm/lightdm.conf
